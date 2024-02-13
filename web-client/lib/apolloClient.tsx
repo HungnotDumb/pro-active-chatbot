0140693f15ab2469e81ccf8edea9581a3836d6c6
HungnotDumb <hungnoyop@proton.me>
2024-02-13 13:21:20 +0000

    chore(release): 4.0.2

---
+++ web-client/lib/apolloClient.tsx
@@ -0,0 +1,28 @@
+
+import { useMemo } from "react";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { __ssrMode__, uri } from "./constants";
+import { createHttpLink } from "@apollo/client/link/http";
+
+let client: ApolloClient<any>;
+
+const createApolloClient = (): ApolloClient<any> => {
+  return new ApolloClient({
+    credentials: "include",
+    ssrMode: __ssrMode__,
+    link: createHttpLink({
+      uri,
+      credentials: "include",
+    }),
+    cache: new InMemoryCache(),
+  });
+};
+export const initializeApollo = (initialState = null) => {
+  const apolloClient = client ?? createApolloClient();
+  if (initialState) {
+    const cache = apolloClient.extract();
+    apolloClient.cache.restore({
+      ...cache,
+      ...initialState,
+    });
+  }
